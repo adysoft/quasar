d84b5eaca9838532edfd8e6c2c8b6bfa70590de5
Yann <yann.alet@gmail.com>
2019-01-11 20:31:37 -0500

    Fixes for new websites
    Both Resetera and Neogaf have updated their websites. Fixed crawlers accordingly. Optimized thumbnails memory footprint

---
+++ Crawler/ImageProcessing.cs
@@ -0,0 +1,53 @@
+﻿namespace Crawler
+{
+    using System;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.Drawing.Drawing2D;
+    using System.Drawing.Imaging;
+    using System.IO;
+
+    public static class ImageProcessing
+    {
+        public static void MakeThumbnail(string sourceImagePath, string thumbnailPath)
+        {
+            Debug.Assert(File.Exists(sourceImagePath));
+            if (File.Exists(thumbnailPath))
+            {
+                File.Delete(thumbnailPath);
+            }
+
+            var image = new Bitmap(sourceImagePath);
+            float desiredHeight = 225f;
+            float ratio = desiredHeight / image.Height;
+            var thumbnail = ResizeImage(image, Convert.ToInt32(image.Width * ratio), (int)desiredHeight);
+            thumbnail.Save(thumbnailPath);
+
+        }
+
+        private static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
---
+++ Quasar/JobData.cs
@@ -0,0 +1,14 @@
+﻿namespace Quasar
+{
+    public class JobData
+    {
+        public JobData(ScreenshotThread screenshotThread, string tempDirectory)
+        {
+            ScreenshotThread = screenshotThread;
+            TempDirectory = tempDirectory;
+        }
+
+        public ScreenshotThread ScreenshotThread { get; set; }
+        public string TempDirectory { get; set; }
+    }
+}
